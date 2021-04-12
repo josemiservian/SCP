3d69a2731e8ac1bcce04e2cee12c740a5736064c
veropindu <57022347+veropindu@users.noreply.github.com>
2021-04-11 20:12:30 -0400

    Plan de facturacion hasta contado

--- apps/administracion/forms.py
+++ apps/administracion/forms.py
@@ -3,30 +3,33 @@ from django import forms
 
 # Models
 from apps.administracion.models import Facturacion, Pago
+from apps.proyectos.models import Cliente, Contrato
+
+#Forma de Pago
+FORMAPAGO_CHOICES = [
+    ('credito', 'Crédito'),
+    ('contado', 'Contado'),
+    ]
 
 #Facturaciones
 class FormCrearFacturacion(forms.Form):
     
-    detalle = forms.CharField(min_length=3, max_length=30)
-    descripcion = forms.CharField(min_length=3, max_length=60)
-    forma_pago = forms.CharField(min_length=3, max_length=15)
+    cliente = forms.ModelChoiceField(queryset=Cliente.objects.all())
+    contrato = forms.ModelChoiceField(queryset=Contrato.objects.all())
+    forma_pago = forms.CharField(label='Forma Pago', widget=forms.Select(choices=FORMAPAGO_CHOICES))
     fecha_emision = forms.DateField(widget=forms.SelectDateWidget)
-    fecha_vencimiento = forms.DateField(widget=forms.SelectDateWidget)
-    monto_total = forms.FloatField()
-    monto_facturacion = forms.FloatField()
-    saldo_facturacion = forms.FloatField()
-    estado = forms.CharField(min_length=3, max_length=15)
+    monto_total =  forms.FloatField() #aca debo poner una funcion que me traiga el monto asociado al contrato seleccionado
 
     def save(self):
         """Crea y guarda una factura"""
         data = self.cleaned_data
-        factura = Facturacion(detalle=data['detalle'], descripcion=data['descripcion'],
+        factura = Facturacion(detalle=data['forma_pago'], descripcion=data['forma_pago'],
                               forma_pago=data['forma_pago'], fecha_emision=data['fecha_emision'],
-                              fecha_vencimiento=data['fecha_vencimiento'], 
+                              fecha_vencimiento=data['fecha_emision'], 
                               monto_total=data['monto_total'],
-                              monto_facturacion=data['monto_facturacion'], 
-                              saldo_facturacion=data['saldo_facturacion'],
-                              estado=data['estado'])
+                              monto_facturacion=data['monto_total'], 
+                              saldo_facturacion=data['monto_total'],
+                              estado=data['forma_pago'])
         factura.save()
 
 
